7427c7073af6b08a2a4fc24bc297ab8d03b1b127
Bubalehich <janeridebmx@gmail.com>
2023-11-27 21:18:41 +0300

    feat:add proxy repo

---
+++ src/main/java/ru/clevertec/repository/impl/ProxyRepositoryInMemory.java
@@ -0,0 +1,41 @@
+package ru.clevertec.repository.impl;
+
+import lombok.RequiredArgsConstructor;
+import ru.clevertec.cache.Cache;
+import ru.clevertec.entity.Entity;
+
+import java.util.List;
+import java.util.Optional;
+import java.util.UUID;
+
+@RequiredArgsConstructor
+public class ProxyRepositoryInMemory extends InMemoryEntityRepository {
+    private InMemoryEntityRepository repository;
+    private Cache<UUID, Entity> cache;
+
+    @Override
+    public Optional<Entity> findById(UUID id) {
+        return Optional.ofNullable(cache.get(id)).isPresent()
+                ? Optional.ofNullable(cache.get(id))
+                : repository.findById(id);
+    }
+
+    @Override
+    public List<Entity> findAll() {
+        return repository.findAll();
+    }
+
+    @Override
+    public Entity save(Entity entity) {
+        entity = repository.save(entity);
+        cache.put(entity.getId(), entity);
+
+        return entity;
+    }
+
+    @Override
+    public void delete(UUID id) {
+        repository.delete(id);
+        cache.remove(id);
+    }
+}
